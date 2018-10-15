39e093880c0b055b9c42d3b65903a05617e03bf5
Christian Schmitt <christian.c.schmitt@daimler.com>
2018-10-15 15:32:50 +0200

    added urbandictionary query

---
+++ src/main/java/org/matterbot/services/urbandictionary/MatterbotUrbanDictionaryResource.java
@@ -0,0 +1,36 @@
+package org.matterbot.services.urbandictionary;
+
+import org.matterbot.mattermost.MattermostService;
+import org.matterbot.services.URLQueryService;
+import org.matterbot.services.giphy.GiphyMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class MatterbotUrbanDictionaryResource {
+    private URLQueryService urbanDictionaryService;
+
+    private MattermostService mattermostService;
+
+    @Autowired
+    private MatterbotUrbanDictionaryResource(MattermostService mattermostService, URLQueryService urbanDictionaryService){
+        this.mattermostService = mattermostService;
+        this.urbanDictionaryService = urbanDictionaryService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = {"/call/urbandict/search"})
+    public ResponseEntity<String> postUrbanSearchResult(@RequestParam("query") String query) throws IOException{
+        String urbanUrl = urbanDictionaryService.getUrl(URLQueryService.Strategy.SEARCH, query);
+        UrbanDictionaryMessage urbanMessage = UrbanDictionaryMessage.builder()
+                .caption(query)
+                .description(urbanUrl)
+                .build();
+        return mattermostService.sendMessage(urbanMessage);
+    }
+}
---
+++ src/main/java/org/matterbot/services/urbandictionary/UrbanDictionaryMessage.java
@@ -0,0 +1,21 @@
+package org.matterbot.services.urbandictionary;
+
+import lombok.Builder;
+import lombok.Data;
+import org.matterbot.mattermost.MattermostMessage;
+
+@Data
+@Builder
+public class UrbanDictionaryMessage implements MattermostMessage {
+
+    private String description;
+
+    private String caption;
+
+    private final String NEWLINE = "\n";
+
+    @Override
+    public String getMessage() {
+        return caption + NEWLINE + description;
+    }
+}
